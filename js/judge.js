feab728199801fa2d6d9d597853e99c1deb65a55
schia <schiaaa333@gmail.com>
2019-10-19 20:51:28 +0900

    Merge branch 'dev' of https://github.com/yoshiakiimaizumi/tetris into newStart
    # Conflicts:
    #	index.html

--- js/judge.js
+++ js/judge.js
@@ -51,5 +51,57 @@ function poseShow(){
 function poseHide(){
     $(".pose").hide();
     console.log("hide");
-
 }
+
+//フルスクリーンスイッチ(要検討)
+    const fullBtn = $('#fullScreenSwitch');
+
+  
+    const btn = document.querySelector('#btn');
+    btn.addEventListener('click', (event) => {
+    // フルスクリーンにする
+     myReqeustFullScreen(document.body);
+    });
+
+    function myReqeustFullScreen(element) {
+        if (element.webkitRequestFullscreen) {
+            //標準仕様
+            element.requestFullscreen();
+        }else if (element.webkitRequestFullscreen) {
+            // Safari, Chrome
+            element.webkitRequestFullscreen();
+        } else if (element.mozRequestFullScreen) {
+            // Firefox
+            element.mozRequestFullScreen();
+        } else if (element.msRequestFullscreen) {
+            // IE11+
+            element.msRequestFullscreen();
+        }else if(element.webkitRequestFullscreen) {
+            // 標準仕様
+            element.requestFullscreen();
+        }  
+    }
+
+    const btnExit = document.querySelector('#btnExit');
+    btnExit.addEventListener('click', (event) => {
+    // フルスクリーンを解除する
+    myCancelFullScreen();
+    });
+
+    function myCancelFullScreen() {
+    if (document.exitFullscreen) {
+        // 標準仕様
+        document.exitFullscreen();
+    } else if (document.webkitCancelFullScreen) {
+        // Safari, Chrome
+        document.webkitCancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+        // Firefox
+        document.mozCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+        // IE 11+
+        document.msExitFullscreen();
+    }
+    }
+      
+
